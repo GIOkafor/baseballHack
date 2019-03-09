08134fb4755c9a16770eb357a1b8dd0e2de292f5
Jin Yoo <jin@knowho.ca>
2019-03-09 15:36:27 -0500

    adding food mock data

--- src/app/menu-list/menu-list.component.ts
+++ src/app/menu-list/menu-list.component.ts
@@ -9,51 +9,75 @@ export class MenuListComponent implements OnInit {
 	foodItems = [
 		{
 			id: 0,
-			name: 'Mucho Nachos',
-			price: 13.5,
-			image: ''
+			name: 'Hot Dog',
+			price: 8.5,
+			image: 'assets/F1.jpg'
 		},
 		{
 			id: 1,
-			name: 'Hot Dog',
-			price: 8.5,
-			image: ''
+			name: 'Mucho Nachos',
+			price: 13.5,
+			image: 'assets/F2.jpg'
+		},
+		{
+			id: 2,
+			name: 'Chips with Cheese',
+			price: 11.5,
+			image: 'assets/F3.jpg'
+		},
+		{
+			id: 3,
+			name: 'Salad Bowl',
+			price: 12.5,
+			image: 'assets/F4.jpg'
+		},
+		{
+			id: 4,
+			name: 'Beef Burrito',
+			price: 12.5,
+			image: 'assets/F5.jpg'
+		},
+		{
+			id: 5,
+			name: 'Chicken Quesadillas',
+			price: 13.5,
+			image: 'assets/F6.jpg'
 		}
 	];
 
 	drinkItems = [
 		{
-			id: 3,
+			id: 6,
 			name: 'Bud Light',
 			price: 10.5,
 			image: 'assets/D1.jpg'
 		},
 		{
-			id: 4,
+			id: 7,
 			name: 'Steam Whistle',
 			price: 10.5,
 			image: 'assets/D2.jpg'
 		},
 		{
-			id: 5,
+			id: 8,
 			name: 'Coors Light',
 			price: 10.5,
 			image: 'assets/D3.jpg'
 		},
 		{
-			id: 6,
+			id: 9,
 			name: 'Coca Cola',
 			price: 8.5,
 			image: 'assets/D4.jpg'
 		},
 		{
-			id: 7,
+			id: 10,
 			name: 'Ginger Ale',
 			price: 8.5,
 			image: 'assets/D5.jpg'
 		},
 		{
-			id: 8,
+			id: 11,
 			name: 'Lemonade',
 			price: 8.5,
 			image: 'assets/D6.jpg'
