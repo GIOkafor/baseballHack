fd73e2987c5edc6e98677e2dc02ed85f7d39caea
Ikechukwu Okafor <ikechukwuokafor@hotmail.com>
2019-03-09 11:05:58 -0500

    scaffolding done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MenuListComponent } from './menu-list/menu-list.component';
+import { MenuDetailsComponent } from './menu-details/menu-details.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+
+//routing stuff
+const appRoutes: Routes = [
+  { path: 'home', component: AppComponent },
+  { path: 'menu-list', component: MenuListComponent },
+  { path: 'menu-details', component: MenuDetailsComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuListComponent,
+    MenuDetailsComponent,
+    CartComponent,
+    CheckoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
