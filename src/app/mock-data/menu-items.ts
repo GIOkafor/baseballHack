2073558252bf0394fb16d5b7f3acef572de57d71
Ikechukwu Okafor <ikechukwuokafor@hotmail.com>
2019-03-09 15:13:29 -0500

    index bug fixed

--- src/app/mock-data/menu-items.ts
+++ src/app/mock-data/menu-items.ts
@@ -1,52 +1,58 @@
 export const MENU = [
     {
-        'id': 0,
-        'name': 'Mucho Nachos',
-        'price': 13.50,
-        'image': ''
+        id: 0,
+        name: 'Mucho Nachos',
+        price: 13.50,
+        image: ''
     },
     {
-        'id': 1,
-        'name': 'Hot Dog',
-        'price': 8.50,
-        'image': ''
+        id: 1,
+        name: 'Hot Dog',
+        price: 8.50,
+        image: ''
     },
     {
-        'id': 3,
-        'name': 'Bud Light',
-        'price': 10.50,
-        'image': 'assets/d1.jpg'
-      },
-      {
-        'id': 4,
-        'name': 'Steam Whistle',
-        'price': 10.50,
-        'image': 'assets/d2.jpg'
-      },
-      {
-        'id': 5,
-        'name': 'Coors Light',
-        'price': 10.50,
-        'image': 'assets/d3.jpg'
-      },
-      {
-        'id': 6,
-        'name': 'Coca Cola',
-        'price': 8.50,
-        'image': 'assets/d4.jpg'
-      },
-      {
-        'id': 7,
-        'name': 'Ginger Ale',
-        'price': 8.50,
-        'image': 'assets/d5.jpg'
-      },
-      {
-        'id': 8,
-        'name': 'Lemonade',
-        'price': 8.50,
-        'image': 'assets/d6.jpg'
-      },
+        id: 2,
+        name: 'New Item',
+        price: 8.50,
+        image: ''
+    },
+    {
+        id: 3,
+        name: 'Bud Light',
+        price: 10.5,
+        image: 'assets/D1.jpg'
+    },
+    {
+        id: 4,
+        name: 'Steam Whistle',
+        price: 10.5,
+        image: 'assets/D2.jpg'
+    },
+    {
+        id: 5,
+        name: 'Coors Light',
+        price: 10.5,
+        image: 'assets/D3.jpg'
+    },
+    {
+        id: 6,
+        name: 'Coca Cola',
+        price: 8.5,
+        image: 'assets/D4.jpg'
+    },
+    {
+        id: 7,
+        name: 'Ginger Ale',
+        price: 8.5,
+        image: 'assets/D5.jpg'
+    },
+    {
+        id: 8,
+        name: 'Lemonade',
+        price: 8.5,
+        image: 'assets/D6.jpg'
+    },
       {
         'id': 9,
         'name': 'Chips with Cheese',
