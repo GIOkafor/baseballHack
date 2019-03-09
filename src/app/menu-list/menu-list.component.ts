70bb09cee2f1c14590e1a6d0cb2e47479ccfc38d
Ikechukwu Okafor <ikechukwuokafor@hotmail.com>
2019-03-09 14:18:22 -0500

    dynamic menu items added

--- src/app/menu-list/menu-list.component.ts
+++ src/app/menu-list/menu-list.component.ts
@@ -7,6 +7,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuListComponent implements OnInit {
 
+  foodItems = [
+    {
+      'name': 'Mucho Nachos',
+      'price': 13.50,
+      'image': ''
+    },
+    {
+      'name': 'Hot Dog',
+      'price': 8.50,
+      'image': ''
+    }
+  ];
+
+  drinkItems = [
+    {
+      'name': 'Bud Light',
+      'price': 10.50,
+      'image': 'assets/d1.jpg'
+    },
+    {
+      'name': 'Steam Whistle',
+      'price': 10.50,
+      'image': 'assets/d2.jpg'
+    },
+    {
+      'name': 'Coors Light',
+      'price': 10.50,
+      'image': 'assets/d3.jpg'
+    },
+    {
+      'name': 'Coca Cola',
+      'price': 8.50,
+      'image': 'assets/d4.jpg'
+    },
+    {
+      'name': 'Ginger Ale',
+      'price': 8.50,
+      'image': 'assets/d5.jpg'
+    },
+    {
+      'name': 'Lemonade',
+      'price': 8.50,
+      'image': 'assets/d6.jpg'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
